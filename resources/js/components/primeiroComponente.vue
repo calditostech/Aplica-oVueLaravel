2081b94189d729ab5d70ab8130cd6fdd9621b4c2
Ricardo Moreira <calditos1988@outlook.com>
2021-02-11 22:43:56 -0300

    first commit

---
+++ resources/js/components/primeiroComponente.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Primeiro Componente com Laravel</h2>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+h2{
+  color: chartreuse;
+}
+</style>
